6174be7b5a9f353dd0d1c85076926890c6198a45
jasongaraygaykhaw <jason.garaygay.khaw@gmail.com>
2026-07-02 00:53:51 +0800

    resolution on PMD findings and added ruleset in pom

--- src/main/java/com/example/stocksync/StockSyncServiceApplication.java
+++ src/main/java/com/example/stocksync/StockSyncServiceApplication.java
@@ -6,10 +6,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-
+@SuppressWarnings("PMD.UseUtilityClass")
 public class StockSyncServiceApplication {
 
+    public StockSyncServiceApplication() {
 
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(StockSyncServiceApplication.class, args);
