fc4e2f506749024d9c4aa0fd0deb3eb3862cb543
jasongaraygaykhaw <jason.garaygay.khaw@gmail.com>
2026-07-02 15:40:51 +0800

    added info logging to monitor scheduled sync, also integrated maven build in dockerization

--- src/main/java/com/example/stocksync/service/StockSyncScheduler.java
+++ src/main/java/com/example/stocksync/service/StockSyncScheduler.java
@@ -1,10 +1,14 @@
 package com.example.stocksync.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StockSyncScheduler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockSyncScheduler.class);
 
     private final StockSyncService stockSyncService;
 
@@ -14,6 +18,14 @@ public class StockSyncScheduler {
 
     @Scheduled(fixedDelayString = "${stock-sync.schedule-ms}")
     public void runScheduledSync() {
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Scheduled stock synchronization started");
+        }
+
         stockSyncService.synchronize();
+
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Scheduled stock synchronization completed");
+        }
     }
 }
